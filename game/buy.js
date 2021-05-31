385e681a41a03fed9d52a19f02f42c4c7428ccf3
BlackyChan1800 <76032223+BlackyChan1800@users.noreply.github.com>
2021-05-31 17:08:56 +0800

    Error fix for buying items and coinflip breakdown
    Changed coinflip to only need one embed that is edited for each result.
    
    Also added for if they don't respond before the time limit they lose the coin.
    
    Fixed buying an item not working but trying to implement an amount of items to multiply instead of one at a time.

--- game/buy.js
+++ game/buy.js
@@ -1,14 +1,11 @@
-const Discord = require('discord.js');
-
 const inventoryModel = require('../models/inventorySchema');
-
 const profileModel = require('../models/profileSchema');
 
 module.exports = {
     name: "buy",
     description: "Buy an item from the store",
 
-    async execute(message, args, cmd, client, discord, profileData) {
+    async execute(message, args, profileData) {
 
         var user = message.author
         const inventoryData = await inventoryModel.findOne({ userID: user.id });
@@ -16,9 +13,12 @@ module.exports = {
         if (!inventoryData) return message.channel.send("This user doesn't exist");
         if (!user.id) return message.channel.send("There was an arror finding your discord id");
 
-        var gold = profileData.gold
+        var gold = profileData.gold //Players gold
 
-        var bought_item = args.slice(0).join(" ").toLowerCase()
+        var bought_item = args.slice(0).join(" ").toLowerCase() //Name of the item
+        var amount_item = Math.round(args[1]) //For multiple items
+
+        if (isNaN(amount_item)) return message.channel.send('Please specify a real amount of the item you would like to buy')
 
         let items = {
             "life potion": {
@@ -41,14 +41,14 @@ module.exports = {
 
         if (bought_item in items) {
             let item = items[bought_item]
-            if (gold < item["gold"]) return message.channel.send('You cannot afford to buy this item')
-            await profileModel.findByIdAndUpdate(
+            if (gold < items["cost"]) return message.channel.send('You cannot afford to buy this item')
+            await profileModel.findOneAndUpdate(
                 {
                     userID: user.id
                 },
                 {
                     $inc: {
-                        gold: item["gold"] * -1
+                        gold: item["cost"] //Find a way to make the cost take away for amount of items not just one
                     }
                 }
             );
@@ -58,7 +58,7 @@ module.exports = {
                 },
                 {
                     $push: {
-                        items: item["icon"]
+                        items: item["icon"] //Find a way to push the amount of items players buy not just one
                     }
                 }
             );
