54e4f9ed50e6effb2d94055554f5d3dd0a48d1c6
Mike A. Trethewey <miketrethewey@gmail.com>
2021-06-10 13:46:40 -0700

    Add Team Lists

--- handlers/rosters_handler.js
+++ handlers/rosters_handler.js
@@ -34,8 +34,35 @@ module.exports = (client, Discord) => {
     let dir = "./rosters"
     const rosters_profiles = walk(dir)
 
+    let roster_aliases = {}
+
+    let emojiIDs = {
+        "745409743593406634": { // VillainsBot
+            "apex":         "852638910751309865",
+            "csgo":         "852609157508890675",
+            "r6s":          "852609196973228043",
+            "rocketleague": "852640960512917604",
+            "val":          "852599150654783529"
+        },
+        "788021898146742292": { // Villains Esports
+            "apex":         "852638839071572018",
+            "csgo":         "852609659176484930",
+            "r6s":          "852609684292108329",
+            "rocketleague": "852641027345088533",
+            "val":          "852596728732581899"
+        }
+    }
+
     for (const file of rosters_profiles) {
         let profile = JSON.parse(fs.readFileSync(file, "utf8"))
+        if(file.indexOf("teams") != -1) {
+            let parts = file.split("/")
+            let gameID = parts[parts.length - 2]
+            if(Object.keys(roster_aliases).indexOf(gameID) == -1) {
+                roster_aliases[gameID] = {}
+            }
+            roster_aliases[gameID][profile.aliases[0]] = profile.title
+        }
 
         let stripe = profile["stripe"]
 
@@ -111,75 +138,67 @@ module.exports = (client, Discord) => {
                 description: profile.title + " Schedule",
                 async execute(message, client, Discord) {
 
-                    let req = dasu.req
+                    if(profile.team) {
+                        let req = dasu.req
 
-                    let params = {
-                        method: 'GET',
-                        protocol: 'http',
-                        hostname: 'villainsoce.mymm1.com',
-                        port: 80,
-                        path: '/team/' + profile.team.teamID + '.json',
-                    }
-
-                    req(params, function (err, res, data) {
-                        let json = JSON.parse(data);
-                        let game_details = json["events"];
-
-                        let newEmbed = new MessageEmbed()
-                            .setColor(profile.stripe)
-                            .setTitle("***" + profile.title + " Schedule***")
-                            .setURL("http://villainsoce.mymm1.com/team/" + profile.team.teamID)
-                            // .setDescription()
-                            .setThumbnail(defaults.thumbnail)
-                            .setFooter(defaults.footer.msg, defaults.footer.image)
-                            .setTimestamp();
-
-                        let fields = []
-                        for(let [timestamp,match] of Object.entries(game_details)) {
-                            let name = "";
-                            let value = "";
-                            if(match.discord.status == "complete") {
-                                name += "✅";
-                                value += "Started";
-                            } else {
-                                let emojiName = match.discord.game;
-                                let icons = {
-                                    "745409743593406634": {
-                                        "csgo": "852599076541169694",
-                                        "val": "852599150654783529"
-                                    },
-                                    "788021898146742292": {
-                                        "csgo": "852596632335548417",
-                                        "val": "852596728732581899"
-                                    }
-                                }
-                                if(emojiName == "val") {
-                                    emojiName = "valorant";
-                                }
-                                name += "<:" + emojiName + ":" + icons[message.guild.id][match.discord.game] + ">";
-                                value += "Starting";
-                            }
-                            name += match.discord.team + " <:vs:839624205766230026> " + match.discord.opponent;
-                            value += ": " + match.discord.starting + "\n";
-                            if(match.discord.status == "incomplete" || (match.discord.scoreKeys.bySide.home != 0 || match.discord.scoreKeys.bySide.opponent != 0)) {
-                                value += '[';
-                                if(match.discord.status == "complete") {
-                                    value += "Final ";
-                                }
-                                value += "Score: " + match.discord.scoreKeys.bySide.home + " - " + match.discord.scoreKeys.bySide.opponent;
-                                value += "](" + match.discord.url + ")";
-                            }
-                            fields.push(
-                                {
-                                    name: name,
-                                    value: value
-                                }
-                            )
+                        let params = {
+                            method: 'GET',
+                            protocol: 'http',
+                            hostname: 'villainsoce.mymm1.com',
+                            port: 80,
+                            path: '/team/' + profile.team.teamID + '.json',
                         }
 
-                        newEmbed.addFields(fields);
-                        message.channel.send(newEmbed);
-                    });
+                        req(params, function (err, res, data) {
+                            let json = JSON.parse(data);
+                            let game_details = json["events"];
+
+                            let newEmbed = new MessageEmbed()
+                                .setColor(profile.stripe)
+                                .setTitle("***" + profile.title + " Schedule***")
+                                .setURL("http://villainsoce.mymm1.com/team/" + profile.team.teamID)
+                                // .setDescription()
+                                .setThumbnail(defaults.thumbnail)
+                                .setFooter(defaults.footer.msg, defaults.footer.image)
+                                .setTimestamp();
+
+                            let fields = []
+                            for(let [timestamp,match] of Object.entries(game_details)) {
+                                let name = "";
+                                let value = "";
+                                if(match.discord.status == "complete") {
+                                    name += ((match.discord.winner == match.discord.team) ? "✅" : "❌");
+                                    value += "Started";
+                                } else {
+                                    let emojiName = match.discord.game;
+                                    if(emojiName == "val") {
+                                        emojiName = "valorant";
+                                    }
+                                    name += "<:" + emojiName + ":" + emojiIDs[message.guild.id][match.discord.game] + ">";
+                                    value += "Starting";
+                                }
+                                name += match.discord.team + " <:vs:839624205766230026> " + match.discord.opponent;
+                                value += ": " + match.discord.starting + "\n";
+                                if(match.discord.status == "incomplete" || (match.discord.scoreKeys.bySide.home != 0 || match.discord.scoreKeys.bySide.opponent != 0)) {
+                                    value += '[';
+                                    if(match.discord.status == "complete") {
+                                        value += "Final ";
+                                    }
+                                    value += "Score: " + match.discord.scoreKeys.bySide.home + " - " + match.discord.scoreKeys.bySide.opponent;
+                                    value += "](" + match.discord.url + ")";
+                                }
+                                fields.push(
+                                    {
+                                        name: name,
+                                        value: value
+                                    }
+                                )
+                            }
+
+                            newEmbed.addFields(fields);
+                            message.channel.send(newEmbed);
+                        });
+                    }
                 }
             }
         }
@@ -189,5 +208,44 @@ module.exports = (client, Discord) => {
                 client.commands.set(roster.name + " Schedule", schedule);
             }
         }
+    }
+    if(roster_aliases) {
+        let profile = {
+          title: "Team Lists",
+          aliases: [ "teams" ]
+        }
+        let teams = {
+            name: profile.title,
+            aliases: profile.aliases,
+            description: profile.title,
+            async execute(message, client, Discord) {
+                let newEmbed = new MessageEmbed()
+                    .setColor(profile.stripe)
+                    .setTitle("***" + profile.title + "***")
+                    // .setDescription(JSON.stringify(roster_aliases))
+                    .setThumbnail(defaults.thumbnail)
+                    .setFooter(defaults.footer.msg, defaults.footer.image)
+                    .setTimestamp();
+                let desc = "";
+                for(let [gameID, teams] of Object.entries(roster_aliases)) {
+                    let emojiName = gameID;
+                    if(emojiName == "val") {
+                        emojiName = "valorant";
+                    }
+                    for(let teamID in teams) {
+                        if(teams.hasOwnProperty(teamID)) {
+                            let teamName = teams[teamID];
+                            desc += "<:" + emojiName + ":" + emojiIDs[message.guild.id][gameID] + ">";
+                            desc += teamName + " (`" + teamID + "`)";
+                            desc += "\n";
+                        }
+                    }
+                    desc += "\n";
+                }
+                newEmbed.setDescription(desc);
+                message.channel.send(newEmbed);
+            }
+        }
+        client.commands.set(profile.title, teams);
     }
 }
