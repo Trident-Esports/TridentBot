8988f79b35540438e68dda330d406854276d54f1
Mike A. Trethewey <miketrethewey@gmail.com>
2021-06-10 10:36:55 -0700

    Add Schedule support

--- handlers/rosters_handler.js
+++ handlers/rosters_handler.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+const fs = require('fs')
 const path = require("path")
+const dasu = require('dasu')
 
 const Discord = require('discord.js')
 const {
@@ -102,10 +103,91 @@ module.exports = (client, Discord) => {
                 message.channel.send(newEmbed);
             }
         }
+        let schedule = null
+        if(file.indexOf("teams") !== -1) {
+            schedule = {
+                name: profile.title + " Schedule",
+                aliases: profile.aliases[0] + 's',
+                description: profile.title + " Schedule",
+                async execute(message, client, Discord) {
+
+                    let req = dasu.req
+
+                    let params = {
+                        method: 'GET',
+                        protocol: 'http',
+                        hostname: 'villainsoce.mymm1.com',
+                        port: 80,
+                        path: '/team/' + profile.team.teamID + '.json',
+                    }
+
+                    req(params, function (err, res, data) {
+                        let json = JSON.parse(data);
+                        let game_details = json["events"];
+
+                        let newEmbed = new MessageEmbed()
+                            .setColor(profile.stripe)
+                            .setTitle("***" + profile.title + " Schedule***")
+                            .setURL("http://villainsoce.mymm1.com/team/" + profile.team.teamID)
+                            // .setDescription()
+                            .setThumbnail(defaults.thumbnail)
+                            .setFooter(defaults.footer.msg, defaults.footer.image)
+                            .setTimestamp();
+
+                        let fields = []
+                        for(let [timestamp,match] of Object.entries(game_details)) {
+                            let name = "";
+                            let value = "";
+                            if(match.discord.status == "complete") {
+                                name += "✅";
+                                value += "Started";
+                            } else {
+                                let emojiName = match.discord.game;
+                                let icons = {
+                                    "745409743593406634": {
+                                        "csgo": "852599076541169694",
+                                        "val": "852599150654783529"
+                                    },
+                                    "788021898146742292": {
+                                        "csgo": "852596632335548417",
+                                        "val": "852596728732581899"
+                                    }
+                                }
+                                if(emojiName == "val") {
+                                    emojiName = "valorant";
+                                }
+                                name += "<:" + emojiName + ":" + icons[message.guild.id][match.discord.game] + ">";
+                                value += "Starting";
+                            }
+                            name += match.discord.team + " <:vs:839624205766230026> " + match.discord.opponent;
+                            value += ": " + match.discord.starting + "\n";
+                            if(match.discord.status == "incomplete" || (match.discord.scoreKeys.bySide.home != 0 || match.discord.scoreKeys.bySide.opponent != 0)) {
+                                value += '[';
+                                if(match.discord.status == "complete") {
+                                    value += "Final ";
+                                }
+                                value += "Score: " + match.discord.scoreKeys.bySide.home + " - " + match.discord.scoreKeys.bySide.opponent;
+                                value += "](" + match.discord.url + ")";
+                            }
+                            fields.push(
+                                {
+                                    name: name,
+                                    value: value
+                                }
+                            )
+                        }
+
+                        newEmbed.addFields(fields);
+                        message.channel.send(newEmbed);
+                    });
+                }
+            }
+        }
         if (roster.name) {
             client.commands.set(roster.name, roster);
-        } else {
-            continue;
+            if(schedule) {
+                client.commands.set(roster.name + " Schedule", schedule);
+            }
         }
     }
 }
