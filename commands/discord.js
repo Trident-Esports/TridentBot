9663e9f670b074bbe6bb92015948397755c26efd
Mike A. Trethewey <miketrethewey@gmail.com>
2021-06-24 13:37:01 -0700

    Turn off old Discord Invite

--- commands/discord.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'discord',
-    description: "This is to join the bot discord server",
-    execute(message) {
-        DISCORD_INVITE = "Gh2Jh5FpVS"
-        message.channel.send('https://discord.gg/' + DISCORD_INVITE);
-    }
-}
