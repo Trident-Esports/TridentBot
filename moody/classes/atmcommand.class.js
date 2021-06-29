dacaa2b770d3c2d4d03f4ef343771ad34679ba21
Mike A. Trethewey <miketrethewey@gmail.com>
2021-06-29 12:29:10 -0700

    Update Give

--- moody/classes/atmcommand.class.js
+++ moody/classes/atmcommand.class.js
@@ -25,6 +25,7 @@ module.exports = class ATMCommand extends GameCommand {
         }
 
         const user = message.author
+        const target = message.mentions.members.first()
         const loaded = user
 
         props.players.user = {
@@ -54,8 +55,52 @@ module.exports = class ATMCommand extends GameCommand {
                 props.description = "User not found."
             }
 
+            let targetData = null
+            if (props.caption.text == "Give") {
+                if (target) {
+                    if (user.id === target.id) {
+                        props.title.text = "Error"
+                        props.description = "You can't give Gold to yourself!"
+                    }
+                    if (target?.user?.bot && target.user.bot) {
+                        props.title.text = "Error"
+                        props.description = this.errors.cantActionBot.join("\n")
+                    }
+
+                    if (props.title.text != "Error") {
+                        props.players.target = {
+                            name: target.username,
+                            avatar: target.user.displayAvatarURL({ format: "png", dynamic: true })
+                        }
+                        targetData = await this.profileModel.findOne({
+                            userID: target.id
+                        })
+
+                        if (!targetData) {
+                            props.title.text = "Error"
+                            props.description = "Target not found."
+                        }
+                    }
+                } else {
+                    props.title.text = "Error"
+                    props.description = "You need to mention a player to give them Gold."
+                }
+            }
+
             if (props.title.text != "Error") {
-                let reserve = ((props.title.text == "Deposit") ? profileData.gold : profileData.bank)
+                let reserve = 0
+                switch (props.caption.text) {
+                    case "Deposit":
+                    case "Give":
+                        reserve = profileData.gold
+                        break
+                    case "Withdraw":
+                        reserve = profileData.bank
+                        break
+                    default:
+                        reserve = 0
+                        break
+                }
                 if (amount == 'all') {
                     amount = parseInt(reserve)
                 } else if (amount == 'half') {
@@ -69,18 +114,44 @@ module.exports = class ATMCommand extends GameCommand {
                 }
 
                 if (props.title.text != "Error") {
-                    let inc = ((props.title.text == "Deposit") ? { gold: -amount, bank: amount } : { gold: amount, bank: -amount })
+                    let inc = {}
+                    let targetInc = {}
+                    let [verb, direction, container] = ["", "", ""]
+                    switch (props.caption.text) {
+                        case "Deposit":
+                            inc = { gold: -amount, bank: amount };
+                            [verb, direction, container] = ["Deposited", "into", "their Bank"];
+                            break;
+                        case "Give":
+                            inc = { gold: -amount };
+                            targetInc = { gold: amount };
+                            [verb, direction, container] = ["Given", "to", `<@${target.id}>'s Wallet`];
+                            break;
+                        case "Withdraw":
+                            inc = { gold: amount, bank: -amount };
+                            [verb, direction, container] = ["Withdrawn", "into", "their Wallet"];
+                            break;
+                        default:
+                            break;
+                    }
                     await this.profileModel.findOneAndUpdate({
-                            userID: message.author.id
+                            userID: user.id
                         }, {
-                            $inc: inc,
+                            $inc: inc
                         }
                     );
+                    if (props.caption.text == "Give" && target) {
+                        await this.profileModel.findOneAndUpdate({
+                                userID: target.id
+                            }, {
+                                $inc: targetInc
+                            }
+                        );
+                    }
 
                     props.description = []
 
-                    let [verb, direction] = ((props.title.text == "Deposit") ? [ "Deposited", "into" ] : [ "Withdrawn", "from" ])
-                    props.description.push(`**${message.author} has ${verb} ${this.emojis.gold}${amount.toLocaleString("en-AU")} Gold ${direction} their Bank!**`)
+                    props.description.push(`**<@${user.id}> has ${verb} ${this.emojis.gold}${amount.toLocaleString("en-AU")} Gold ${direction} ${container}!**`)
                     props.description.push("_Check your balance using `.balance`_")
 
                     props.description = props.description.join("\n")
