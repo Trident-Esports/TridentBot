7340935387fd6357b2fedf91d2466cfc1fde20fa
Mike A. Trethewey <miketrethewey@gmail.com>
2021-06-29 12:35:54 -0700

    Update Inventory

--- moody/commands/gamecommands/inventory.js
+++ moody/commands/gamecommands/inventory.js
@@ -15,58 +15,73 @@ module.exports = class InventoryCommand extends GameCommand {
 
     async run(client, message, args) {
         let props = {
-            title: {
+            caption: {
                 text: "Inventory"
             },
+            title: {},
             description: "",
             footer: {
                 msg: ""
+            },
+            players: {
+                user: {},
+                target: {}
             }
         }
 
-        let mentionedMember = null;
-        if (args.length) {
-            mentionedMember = message.mentions.members.first().user;
-        } else {
-            mentionedMember = message.author;
+        const user = message.author
+        const target = message.mentions.members.first()
+        const loaded = target ? target.user : user
+        props.players.user = {
+            name: user.username,
+            avatar: user.displayAvatarURL({ format: "png", dynamic: true })
         }
 
-        if (!mentionedMember) return message.channel.send("That user does not exist");
-        const inventoryData = await this.inventoryModel.findOne({
-            userID: mentionedMember.id
-        });
+        if (loaded?.bot && loaded.bot) {
+            props.title.text = "Error"
+            props.description = this.errors.cantActionBot.join("\n")
+        }
 
-        let inventory = {}
-        for (let cat of ["items","consumables","powers"]) {
-            if (!(cat in inventory)) {
-                inventory[cat] = {}
-            }
-            for(let item of inventoryData[cat]) {
-                if (!(item in inventory[cat])) {
-                    inventory[cat][item] = 0
+        if (props.title.text != "Error") {
+            if (target) {
+                props.players.target = {
+                    name: target.username,
+                    avatar: target.user.displayAvatarURL({ format: "png", dynamic: true })
                 }
-                inventory[cat][item] += 1
+            }
+
+            const inventoryData = await this.inventoryModel.findOne({
+                userID: loaded.id
+            });
+
+            let inventory = {}
+            for (let cat of ["items","consumables","powers"]) {
+                if (!(cat in inventory)) {
+                    inventory[cat] = {}
+                }
+                for(let item of inventoryData[cat]) {
+                    if (!(item in inventory[cat])) {
+                        inventory[cat][item] = 0
+                    }
+                    inventory[cat][item] += 1
+                }
+            }
+
+            props.description = `<@${loaded.id}>'s Inventory`
+            props.fields = []
+
+            for (let [cat, items] of Object.entries(inventory)) {
+                let value = Object.entries(items).length == 0 ? "Nothing" : ""
+                for (let [item, q] of Object.entries(items)) {
+                    value += item + '`x' + (q + "").padStart(3) + "`\n"
+                }
+                props.fields.push({
+                    name: cat.charAt(0).toUpperCase() + cat.slice(1),
+                    value: value,
+                    inline: true
+                })
             }
         }
-
-        props.description = `${mentionedMember}'s Inventory`
-        props.fields = []
-
-        for (let [cat, items] of Object.entries(inventory)) {
-            let value = Object.entries(items).length == 0 ? "Nothing" : ""
-            for (let [item, q] of Object.entries(items)) {
-                value += item + '`x' + (q + "").padStart(3) + "`\n"
-            }
-            props.fields.push({
-                name: cat.charAt(0).toUpperCase() + cat.slice(1),
-                value: value,
-                inline: true
-            })
-        }
-
-        props.thumbnail = mentionedMember.avatarURL({
-            dynamic: true
-        })
 
         let embed = new VillainsEmbed(props)
         this.send(message, embed);
