8b9f95a48a62a27f28c59a176492782bb79335b7
Mike A. Trethewey <miketrethewey@gmail.com>
2021-07-07 23:39:03 -0700

    Fix Game Help

--- moody/commands/gamecommands/gamehelp.js
+++ moody/commands/gamecommands/gamehelp.js
@@ -5,81 +5,103 @@ const fs = require('fs');
 
 module.exports = class GameHelpCommand extends GameCommand {
     constructor() {
-        super({
+        let comprops = {
             name: 'gamehelp',
             aliases: ['gh'],
             category: 'game',
-            description: 'This is a help embed',
-        });
-    }
-    async run(client, message, args) {
-
+            description: 'This is a help embed'
+        }
         let props = {
-            title: {
+            caption: {
                 text: "Game Help"
-            },
-            description: "",
-            footer: {
-                msg: ""
             }
         }
-
-        let helpData = JSON.parse(fs.readFileSync("game/dbs/help.json", "utf8"))
-        let defaults = JSON.parse(fs.readFileSync("dbs/defaults.json", "utf8"))
-
-        let loadSection = args && args[0] && Object.keys(helpData).indexOf(args[0].toLowerCase()) !== -1;
-        if (loadSection) {
-            helpData = {
-                key: helpData[args[0].toLowerCase()]
-            }
-        }
-
-        for (let [section, sectionAttrs] of Object.entries(helpData)) {
-            let value = sectionAttrs?.help ? sectionAttrs.help : " "
-            if (!loadSection) {
-                let values = [];
-                for (let command in sectionAttrs.commands) {
-                    values.push("`" + command + "`")
-                }
-                value = values.join(", ")
-            }
-            props.fields = [{
-                name: "**" + sectionAttrs.section + "**" + (section != "key" ? " (`" + section + "`)" : ""),
-                value: value,
-                inline: false
-            }]
-
-            if (loadSection) {
-                let shown = false
-                for (let [command, commandAttrs] of Object.entries(sectionAttrs.commands)) {
-                    let show = true
-                    if (args && args[1] && args[1].toLowerCase() !== command) {
-                        show = false
-                    }
-                    if (show) {
-                        shown = true
-                        let value = commandAttrs.help.join("\n")
-                        if ("aliases" in commandAttrs) {
-                            value += "\n"
-                            value += "[Aliases: " + commandAttrs.aliases.join(", ") + "]"
-                        }
-                        props.fields = [{
-                            name: "`" + defaults.prefix + command + "`",
-                            value: value,
-                            inline: false
-                        }]
-                    }
-                }
-                if (!shown && (args && args[1])) {
-                    props.fields = [{
-                        name: "Error",
-                        value: "Command `" + args[1].toLowerCase() + "` not present in `" + sectionAttrs.section + "`.",
-                        inline: false
-                    }]
-                }
-            }
-        }
-        let embed = new VillainsEmbed(props)
-        this.send(message, embed);
+        super(comprops, props)
     }
-} //FIX ME: Only shows last available of each command type
+
+    async action(client, message) {
+        if (!(this.error)) {
+            // Get help data
+            let helpData = JSON.parse(fs.readFileSync("./game/dbs/help.json","utf8"))
+
+            // If we're loading a section
+            let loadSection = this.inputData.args && this.inputData.args[0] && this.inputData.args[0] != "" ? this.inputData.args[0].toLowerCase() : ""
+            if (loadSection != "") {
+                if (Object.keys(helpData).indexOf(loadSection) > -1) {
+                    helpData = {
+                        key: helpData[loadSection]
+                    }
+                } else {
+                    // Section find failed, try to load a subsection
+                    for (let [section, subsection] of Object.entries(helpData)) {
+                        if (Object.keys(subsection.commands).indexOf(loadSection) > -1) {
+                            helpData = {
+                                key: helpData[section]
+                            };
+                            let command = {};
+                            command[loadSection] = helpData.key.commands[loadSection];
+                            helpData.key.commands = command;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            this.props.fields = []
+
+            for (let [section, sectionAttrs] of Object.entries(helpData)) {
+                let value = "** **"
+
+                if (!loadSection) {
+                    let values = []
+                    for (let command in sectionAttrs.commands) {
+                        values.push("`" + command + "`")
+                    }
+                    value = values.join(", ")
+                }
+
+                this.props.fields.push(
+                    {
+                        name: "**" + sectionAttrs.section + "**" + (section != "key" ? " (`" + section + "`)" : "") + "\n" + sectionAttrs.help,
+                        value: value,
+                        inline: false
+                    }
+                )
+
+                if (loadSection) {
+                    let shown = false
+                    for (let [command, commandAttrs] of Object.entries(sectionAttrs.commands)) {
+                        let show = true
+                        if (this.inputData.args && this.inputData.args[1] && this.inputData.args[1].toLowerCase() !== command) {
+                            show = false
+                        }
+                        if (show) {
+                            shown = true
+                            let value = commandAttrs.help.join("\n")
+                            if ("aliases" in commandAttrs) {
+                                value += "\n"
+                                value += "[Aliases: `." + commandAttrs.aliases.join("`, `.") + "`]"
+                            }
+                            this.props.fields.push(
+                                {
+                                    name: "`" + this.prefix + command + "`",
+                                    value: value,
+                                    inline: false
+                                }
+                            )
+                        }
+                    }
+                    if (!(shown) && (this.inputData.args && this.inputData.args[1])) {
+                        this.props.fields.push(
+                            {
+                                name: "Error",
+                                value: "Command `" + this.inputData.args[1].toLowerCase() + "` not present in `" + sectionAttrs.section + "`.",
+                                inline: false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
