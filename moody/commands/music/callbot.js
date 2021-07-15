931f21fd9ff3b59d541b2026c8043ba99304ab1f
Mike A. Trethewey <miketrethewey@gmail.com>
2021-07-14 17:35:28 -0700

    Stub out music stuff

---
+++ moody/commands/music/callbot.js
@@ -0,0 +1,1 @@
+// Call Bot to Voice Channel
--- moody/commands/music/play.js
+++ moody/commands/music/play.js
@@ -23,121 +23,140 @@ module.exports = class PlayCommand extends VillainsCommand {
 
     async action(client, message) {
 
+        // Short circuit if no args
         if (!(this?.inputData?.args)) {
             return;
         }
 
+        // Get voice channel that caller is in
         const voice_channel = message.member.voice.channel;
         if (!voice_channel) {
             this.error = true
             this.props.description = 'You need to be in a voice channel to play some music'
         }
 
-        const permissions = voice_channel.permissionsFor(message.client.user);
-        if ((!(permissions.has('CONNECT' || 'SPEAK')))) {
-            this.error = true
-            this.props.description = 'You dont have access to this channel'
-        }
-
-        const server_queue = queue.get(message.guild.id);
-
-        const video_player = async (guild, song) => {
-            const song_queue = queue.get(guild.id);
-
-            if (!song) {
-                song_queue.voice_channel.leave();
-                queue.delete(guild.id);
-                return;
+        if (!(this.error)) {
+            // Get perms for channel
+            const permissions = voice_channel.permissionsFor(message.client.user);
+            if ((!(permissions.has('CONNECT' || 'SPEAK')))) {
+                this.error = true
+                this.props.description = 'You dont have access to this channel'
             }
 
-            const stream = ytdl(song.url, {
-                filter: 'audioonly'
-            });
+            const server_queue = queue.get(message.guild.id);
 
-            song_queue.connection.play(stream, {
-                seek: 0,
-                volume: 1
-            })
-                .on('finish', () => {
-                    song_queue.songs.shift();
-                    video_player(guild, song_queue.songs[0]);
-                }); {
-                this.props.description = `Now playing ** ${song.title} ** enjoy`
+            const video_player = async (guild, song) => {
+                const song_queue = queue.get(guild.id);
+
+                if (!song) {
+                    song_queue.voice_channel.leave();
+                    queue.delete(guild.id);
+                    return;
+                }
+
+                const stream = ytdl(song.url, {
+                    filter: 'audioonly'
+                });
+
+                song_queue.connection.play(stream, {
+                    seek: 0,
+                    volume: 1
+                })
+                    .on('finish', () => {
+                        song_queue.songs.shift();
+                        video_player(guild, song_queue.songs[0]);
+                    }); {
+                    this.props.description = `Now playing ** ${song.title} ** enjoy`
+                }
             }
-        }
 
-        let song = {};
+            let song = {};
 
-        if (ytdl.validateURL(this?.inputData?.args[0])) {
-            const songInfo = await ytdl.getInfo(this?.inputData?.args[0]);
-            song = {
-                title: songInfo.videoDetails.title,
-                url: songInfo.videoDetails.video_url
-            };
-        } else if (this?.inputData?.args[0].includes('spotify')) {
-            const spotifyTrackInfo = await getPreview(this?.inputData?.args[0]);
-
-            const videoFinder = async (query) => {
-                const videoResult = await ytSearch(query);
-                return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
-            };
-
-            const video = await videoFinder(`${spotifyTrackInfo.title} ${spotifyTrackInfo.artist}`);
-
-            if (video) {
+            // Validate URL as YT URL
+            if (ytdl.validateURL(this?.inputData?.args[0])) {
+                const songInfo = await ytdl.getInfo(this?.inputData?.args[0]);
                 song = {
-                    title: video.title,
-                    url: video.url
+                    title: songInfo.videoDetails.title,
+                    url: songInfo.videoDetails.video_url
                 };
-            } else {
-                this.error = true
-                this.props.description = "Error finding song."
-            }
-        } else {
-            const videoFinder = async (query) => {
-                const videoResult = await ytSearch(query);
-                return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
-            };
-            const video = await videoFinder(this?.inputData?.args.join(''));
+            } else if (this?.inputData?.args[0].includes('spotify')) {
+                // Check if Spotify
+                const spotifyTrackInfo = await getPreview(this?.inputData?.args[0]);
 
-            if (video) {
-                song = {
-                    title: video.title,
-                    url: video.url
+                const videoFinder = async (query) => {
+                    const videoResult = await ytSearch(query);
+                    return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
                 };
+
+                const video = await videoFinder(`${spotifyTrackInfo.title} ${spotifyTrackInfo.artist}`);
+
+                // If we got something, package it
+                if (video) {
+                    song = {
+                        title: video.title,
+                        url: video.url
+                    };
+                } else {
+                    this.error = true
+                    this.props.description = "Error finding song."
+                }
             } else {
-                this.error = true
-                this.props.description = "Error finding song."
+                // Not YT nor Spotify
+                const videoFinder = async (query) => {
+                    const videoResult = await ytSearch(query);
+                    return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
+                };
+                if (this?.inputData?.args.join("").trim() == "") {
+                    this.error = true
+                    this.props.description = "No search string given."
+                }
+                if (!(this.error)) {
+                    // Get the vid
+                    const video = await videoFinder(this?.inputData?.args.join(''));
+
+                    if (video) {
+                        song = {
+                            title: video.title,
+                            url: video.url
+                        };
+                    } else {
+                        this.error = true
+                        this.props.description = "Error finding song."
+                    }
+                }
+            }
+
+            if (!(this.error)) {
+                // If there's no queue
+                if (!server_queue) {
+                    // Make one
+                    const queue_constructor = {
+                        voice_channel: voice_channel,
+                        text_channel: message.channel,
+                        Connection: null,
+                        songs: []
+                    }
+
+                    queue.set(message.guild.id, queue_constructor);
+                    queue_constructor.songs.push(song);
+
+                    try {
+                        const connection = await voice_channel.join();
+                        queue_constructor.connection = connection;
+                        video_player(message.guild, queue_constructor.songs[0]);
+                    } catch (err) {
+                        queue.delete(message.guild.id);
+                        this.error = true
+                        this.props.description = "There was an Error connecting"
+                        throw err;
+                    }
+                } else {
+                    // We've got a queue, add to it
+                    server_queue.songs.push(song); {
+                        this.props.description = `**${song.title}** added to queue`
+                    }
+                }
             }
         }
-
-        if (!server_queue) {
-
-            const queue_constructor = {
-                voice_channel: voice_channel,
-                text_channel: message.channel,
-                Connection: null,
-                songs: []
-            }
-
-            queue.set(message.guild.id, queue_constructor);
-            queue_constructor.songs.push(song);
-
-            try {
-                const connection = await voice_channel.join();
-                queue_constructor.connection = connection;
-                video_player(message.guild, queue_constructor.songs[0]);
-            } catch (err) {
-                queue.delete(message.guild.id);
-                this.error = true
-                this.props.description = "There was an Error connecting"
-                throw err;
-            }
-        } else {
-            server_queue.songs.push(song); {
-                this.props.description = `**${song.title}** added to queue`
-            }
-        }
-
     }
 }
