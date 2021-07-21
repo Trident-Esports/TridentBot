4d1e30090cdfd454be9b886a6838e92c7adf1197
BlackyChan1800 <76032223+BlackyChan1800@users.noreply.github.com>
2021-07-22 04:56:22 +0800

    define desc

--- moody/commands/info/roster.js
+++ moody/commands/info/roster.js
@@ -87,6 +87,7 @@ module.exports = class RosterCommand extends VillainsCommand {
 
             let cachedEmoji = message.guild.emojis.cache.find(emoji => emoji.name === emojiName);
             if (cachedEmoji?.available) {
+                let desc = ""
                 foundEmoji = true
                 desc += `${cachedEmoji}`;
             }
