03d8ceb5616eed659a853a20fe7a04dc4a97de34
Mike A. Trethewey <miketrethewey@gmail.com>
2021-07-21 01:12:28 -0700

    Spiff up Rules Role

--- moody/commands/meta/rulerole.js
+++ moody/commands/meta/rulerole.js
@@ -6,29 +6,32 @@ const fs = require('fs')
 
 module.exports = class RulesRoleCommand extends VillainsCommand {
     constructor() {
-        super({
+        let comprops = {
             name: "rulesrole",
             aliases: [ "rulerole", "rr" ],
             category: "meta",
             description: "Rules Role"
-        })
+        }
+        let props = {
+            caption: {
+                text: "Accepting Values"
+            }
+        }
+        super(comprops, props)
     }
 
     async action(client, message) {
-        let RULES_ROLE = JSON.parse(fs.readFileSync("./dbs/roles.json","utf8"))["rules"]
         let RULES_EMOJI = "✅"
         let RULES_CHANNEL = JSON.parse(fs.readFileSync("./dbs/channels.json"))[message.guild.id]["rules"]
-        RULES_ROLE = message.guild.roles.cache.find(role => role.name === RULES_ROLE)
+        RULES_CHANNEL = message.guild.channels.cache.find(channel => channel.name === RULES_CHANNEL)
         this.channel = RULES_CHANNEL
 
+        this.props.caption = { text: "Accepting Values" }
+
         this.props.description = [
-            `Accepting the values will allow you to interact with the server.`
-        ]
-        this.props.fields = [
-            {
-                name: "***Accepting Values***",
-                value: "By selecting the reaction below, you are agreeing to Villains Values and will be punished according to how severely the values are broken."
-            }
+            `Accepting the values will allow you to interact with the server.`,
+            "",
+            "By selecting the reaction below, you are agreeing to Villains Values and will be punished according to how severely the values are broken."
         ]
 
         this.null = true
