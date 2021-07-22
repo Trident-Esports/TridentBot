029643ab46a6872379ea121e0ae492cfb0a51dfd
BlackyChan1800 <76032223+BlackyChan1800@users.noreply.github.com>
2021-07-22 19:31:22 +0800

    Finish Giveaway

--- moody/commands/fun/giveaway.js
+++ moody/commands/fun/giveaway.js
@@ -137,7 +137,7 @@ module.exports = class GiveawayCommand extends QuestionnaireCommand {
                                 let fields = []
                                 for (let [reaction, reactionData] of collected) {
                                     if (!(reactors[reaction])) {
-                                        reactors[reaction] = []
+                                        reactors[reaction] = [1,2,3,45,21]
                                     }
                                     for (let [userID, ] of reactionData.users.cache) {
                                         if (userID !== client.user.id) {
