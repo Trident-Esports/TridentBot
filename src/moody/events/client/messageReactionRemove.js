ec160498fdd1d014fd5b98c744a6e430f6a23dc2
Mike A. Trethewey <miketrethewey@gmail.com>
2021-09-12 13:42:19 -0700

    More error checking in Member Role Add/Remove

--- src/moody/events/client/messageReactionRemove.js
+++ src/moody/events/client/messageReactionRemove.js
@@ -21,24 +21,45 @@ module.exports = class MessageReactionRemoveEvent extends VillainsEvent {
         }
 
         let RULES_ROLE = JSON.parse(fs.readFileSync("./src/dbs/" + reaction.message.guild.id + "/roles.json","utf8"))["rules"]
-        if (RULES_ROLE) {
-            RULES_ROLE = reaction.message.guild.roles.cache.find(role => role.name === RULES_ROLE)
-            if (RULES_ROLE) {
-                let RULES_EMOJI = "✅"
-                let RULES_CHANNEL = await this.getChannel(reaction.message, "rules")
 
-                if (RULES_CHANNEL?.id) {
-                    if (reaction.message.channel.id == RULES_CHANNEL.id) {
-                        if (reaction.emoji.name === RULES_EMOJI) {
-                            const member = await reaction.message.guild.members.cache.get(user.id)
-                            if (!(member)) {
-                                console.log(`Message Reaction Remove: Member '${user.username}' not found!`)
-                                return
-                            }
-                            member.roles.remove(RULES_ROLE)
-                        }
-                    }
+        if (!(RULES_ROLE)) {
+            console.log("Message Reaction Remove: Guild ID Roles:",reaction.message.guild.id,"not found!")
+            return
+        }
+
+        RULES_ROLE = reaction.message.guild.roles.cache.find(role => role.name === RULES_ROLE)
+        if (!(RULES_ROLE)) {
+            console.log("Message Reaction Remove: Guild Rules Role:",reaction.message.guild.id,"not found!")
+            return
+        }
+
+        let RULES_EMOJI = "✅"
+        let RULES_CHANNEL = await this.getChannel(reaction.message, "rules")
+
+        if (!(RULES_CHANNEL)) {
+            console.log("Message Reaction Remove: Guild ID Rules Channel:",reaction.message.guild.id,"not found!")
+            return
+        }
+
+        if (reaction.message.channel.id == RULES_CHANNEL.id) {
+            if (reaction.emoji.name === RULES_EMOJI) {
+                let member = null
+                try {
+                    member = await reaction.message.guild.members.cache.get(user.id)
+                } catch (err) {
+                    console.log(`Message Reaction Remove: User: '${user.username}#${user.discriminator}' [ID:${user.id}] not found!`)
+                    console.log(err)
+                    return
                 }
+                try {
+                    member.roles.remove(RULES_ROLE)
+                } catch (err) {
+                    console.log(`Message Reaction Remove: Failed to remove Role from User: '${user.username}#${user.discriminator}' [ID:${user.id}!`)
+                    console.log(err)
+                    return
+                }
+            } else {
+                console.log("Message Reaction Remove: Guild ID Rules Channel:",reaction.emoji.name,"!=",RULES_EMOJI)
             }
         }
     }
