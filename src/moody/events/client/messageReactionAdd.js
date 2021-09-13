481d0a1e11d74d11e18be1d6b2d4a9b2bdcd0e07
Mike A. Trethewey <miketrethewey@gmail.com>
2021-09-12 23:04:36 -0700

    More about Reaction Add/Remove

--- src/moody/events/client/messageReactionAdd.js
+++ src/moody/events/client/messageReactionAdd.js
@@ -20,19 +20,6 @@ module.exports = class MessageReactionAddEvent extends VillainsEvent {
             return
         }
 
-        let RULES_ROLE = JSON.parse(fs.readFileSync("./src/dbs/" + reaction.message.guild.id + "/roles.json","utf8"))["rules"]
-
-        if (!(RULES_ROLE)) {
-            console.log("Message Reaction Add: Guild ID Roles:",reaction.message.guild.id,"not found!")
-            return
-        }
-
-        RULES_ROLE = reaction.message.guild.roles.cache.find(role => role.name === RULES_ROLE)
-        if (!(RULES_ROLE)) {
-            console.log("Message Reaction Add: Guild Rules Role:",reaction.message.guild.id,"not found!")
-            return
-        }
-
         let RULES_EMOJI = "✅"
         let RULES_CHANNEL = await this.getChannel(reaction.message, "rules")
 
@@ -43,6 +30,19 @@ module.exports = class MessageReactionAddEvent extends VillainsEvent {
 
         if (reaction.message.channel.id == RULES_CHANNEL.id) {
             if (reaction.emoji.name === RULES_EMOJI) {
+                let RULES_ROLE = JSON.parse(fs.readFileSync("./src/dbs/" + reaction.message.guild.id + "/roles.json","utf8"))["rules"]
+
+                if (!(RULES_ROLE)) {
+                    console.log("Message Reaction Add: Guild ID Roles:",reaction.message.guild.id,"not found!")
+                    return
+                }
+
+                RULES_ROLE = reaction.message.guild.roles.cache.find(role => role.name === RULES_ROLE)
+                if (!(RULES_ROLE)) {
+                    console.log("Message Reaction Add: Guild Rules Role:",reaction.message.guild.id,"not found!")
+                    return
+                }
+
                 let member = null
                 try {
                     member = await reaction.message.guild.members.cache.get(user.id)
