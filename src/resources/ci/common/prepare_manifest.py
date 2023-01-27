284464605f60aac1f133f2e06190af34a197a5d5
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-01-26 19:39:28 -0800

    Update Pages

---
+++ src/resources/ci/common/prepare_manifest.py
@@ -0,0 +1,23 @@
+"""Generate dbs manifest"""
+import json
+import os
+
+files = []
+root = [".","src","rosters","dbs"]
+rdir = ["tdnt"]
+for r,d,f in os.walk(
+  os.path.join(
+    *root,
+    *rdir
+  )
+):
+    for fname in f:
+        if fname not in ["guilds.json","manifest.json"]:
+            files.append(os.path.join(r,fname).replace(os.path.join(*root, ""), ""))
+
+json_obj = json.dumps(files, indent=2)
+
+with open(os.path.join(".","src","rosters","dbs","manifest.json"), "w", encoding="utf-8") as manifest:
+    manifest.write(json_obj)
+
+print(json_obj)
